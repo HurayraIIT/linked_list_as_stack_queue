28de5731f21f8624653061910c760452e74592ed
reduan-masud <redouanmasud@gmail.com>
2020-11-30 14:59:13 +0600

    Bad Input Infinity Loop  Problem fixed

--- Prompt.h
+++ Prompt.h
@@ -1,28 +1,30 @@
 #pragma once
 #include <iostream>
+#include <limits>
 #include "StackModule.h"
 #include "QueueModule.h"
-
+using namespace std;
 // Prompt
 
 void printWelcomeMessage(void)
 {
-    std::cout << std::endl;
-    std::cout << "\t> Welcome stranger!!!!\n";
-    std::cout << "\t> _________-_-________\n";
-    std::cout << std::endl;
+    cout << endl;
+    cout << "\t> Welcome stranger!!!!\n";
+    cout << "\t> _________-_-________\n";
+    cout << endl;
 }
 
 void printInitialChoiceQuery(void)
 {
-    std::cout << "\t> Press \t1\tfor Stack\n";
-    std::cout << "\t> Press \t2\tfor Queue\n";
+    cout << "\t> Press \t1\tfor Stack\n";
+    cout << "\t> Press \t2\tfor Queue\n";
+    cout << "\t> Press \t-1\tfor exit\n";
 }
 
 void printInvalidInputError(void)
 {
-    std::cout << "\n\n\t> Invalid choice.\n";
-    std::cout << "\t> Your choice must be 1 or 2.\n\n";
+    cout << "\n\n\t> Invalid choice.\n";
+    cout << "\t> Your choice must be 1 or 2.\n\n";
 }
 
 bool isChoiceValid (int choice)
@@ -34,15 +36,23 @@ bool isChoiceValid (int choice)
 int takeChoiceInput(void)
 {
     int choice = 0;
-    std::cout << "\t> ";
-    std::cin >> choice; // TODO: Implement better input validation
-    
+    cout << "\t> ";
+    cin >> choice; // TODO: Implement better input validation
+    if(cin.fail())
+    {
+        cout<<"Error -- you did not enter an integer"<<endl;       
+        cin.clear();
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        choice = 0;
+    }
+    if(choice == -1) exit(1);
     if(isChoiceValid(choice)!=true)
     {
         printInvalidInputError();
         printInitialChoiceQuery();
         choice = takeChoiceInput();
     }
+    
     return choice;
 }
 
